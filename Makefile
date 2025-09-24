b982956ff15f1c286a9b69ec0229be636c9140a8
Stan Grishin <stangri@melmac.ca>
2025-09-24 20:01:03 +0000

    IPKG_INSTROOT and ubus fixes

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ include $(TOPDIR)/rules.mk
 
 PKG_NAME:=https-dns-proxy
 PKG_VERSION:=2025.09.11
-PKG_RELEASE:=2
+PKG_RELEASE:=3
 
 PKG_SOURCE_PROTO:=git
 PKG_SOURCE_URL:=https://github.com/baranyaib90/https_dns_proxy/
--- files/etc/uci-defaults/50-https-dns-proxy-migrate-options.sh
+++ files/etc/uci-defaults/50-https-dns-proxy-migrate-options.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+
 sed -i "s|update_dnsmasq_config|dnsmasq_config_update|" "/etc/config/https-dns-proxy"
 sed -i "s|wan6_trigger|procd_trigger_wan6|" "/etc/config/https-dns-proxy"
 sed -i "s|procd_fw_src_interfaces|force_dns_src_interface|" "/etc/config/https-dns-proxy"
